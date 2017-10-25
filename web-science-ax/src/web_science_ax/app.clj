09832e97ff40f934e9e7aa787be7140fef4065d6
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-10-25 19:19:35 +0100

    Added resources for tweets to use as data sets in project

--- web-science-ax/src/web_science_ax/app.clj
+++ web-science-ax/src/web_science_ax/app.clj
@@ -1,6 +1,8 @@
 (ns web-science-ax.app)
+(require '[clojure.data.csv :as csv]
+         '[clojure.java.io :as io])
 
-(defn foo
+(defn- foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
