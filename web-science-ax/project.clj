dcd9879151c56e4745d19b4fd1f4dda9333fff5a
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-10-25 19:17:47 +0100

    Updated info on project

--- web-science-ax/project.clj
+++ web-science-ax/project.clj
@@ -1,6 +1,7 @@
 (defproject web-science-ax "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :author "Andrei-Mihai Nicolae"
+  :description "Web Science (M) assessed exercise"
+  :url "https://github.com/nclandrei/l5-coursework"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
