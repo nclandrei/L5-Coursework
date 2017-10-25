a851d3e5f52fe09948fceb4b875aa95e456157c1
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-10-25 19:38:11 +0100

    Can now read a CSV file and convert it into a map

--- web-science-ax/src/web_science_ax/app.clj
+++ web-science-ax/src/web_science_ax/app.clj
@@ -1,8 +1,12 @@
-(ns web-science-ax.app)
-(require '[clojure.data.csv :as csv]
-         '[clojure.java.io :as io])
+(ns web-science-ax.app
+  (:require [clojure.data.csv :as csv]
+           [clojure.java.io :as io]))
 
-(defn- foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn csv-data->maps [csv-data]
+  "Reads in a CSV file and outputs a map"
+  (map zipmap
+       (->> (first csv-data) ;; First row is the header
+            (map keyword) ;; Drop if you want string keys instead
+            repeat)
+       (rest csv-data)))
+
