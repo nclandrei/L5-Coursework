69be4318c7e077201f80360af9fac3768ef92c3a
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-10-25 20:18:48 +0100

    Added multiple functions to app

--- web-science-ax/src/web_science_ax/app.clj
+++ web-science-ax/src/web_science_ax/app.clj
@@ -2,11 +2,31 @@
   (:require [clojure.data.csv :as csv]
            [clojure.java.io :as io]))
 
+(def csvKeys [:cluster_id :cluster_name_entity
+           :tweet_id :timestamp_ms :user_id :tweet_tokens :tweet_text])
+
 (defn csv-data->maps [csv-data]
-  "Reads in a CSV file and outputs a map"
+  "Reads in a CSV file and outputs a map with the keys defined in csvKeys"
   (map zipmap
-       (->> (first csv-data) ;; First row is the header
-            (map keyword) ;; Drop if you want string keys instead
+       (->> csvKeys
+            (map keyword)
             repeat)
-       (rest csv-data)))
+       csv-data))
+
+(defn group-clusters [docMap]
+  "Groups clusters by cluster_id"
+  (group-by :cluster_id docMap))
+
+(defn compute-mean-time [docs]
+  "Computes mean time for documents in a specific cluster"
+  (/ (reduce + (:timestamp_ms docs)) (count docs)))
+
+(defn add-centroid-times [docMap]
+  "Creates a new map with key as centroid time and value as an array of
+  the corresponding documents for that cluster"
+  )
+
+(defn filter-out-small-clusters [docMap]
+  "Filters out the clusters having less than 10 documents"
+  (filter (fn [[k v]] (> (count v) 10)) docMap))
 
