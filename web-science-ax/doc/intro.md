f9d97aea87c0a2656250f1132149d0f82e8ada5a
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-10-25 19:14:09 +0100

    Skeleton for app included

---
+++ web-science-ax/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to web-science-ax
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ web-science-ax/test/web_science_ax/core_test.clj
@@ -0,0 +1,7 @@
+(ns web-science-ax.core-test
+  (:require [clojure.test :refer :all]
+            [web-science-ax.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
