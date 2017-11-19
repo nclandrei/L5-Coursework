87fe6bfb2d4032578c3c1138270250b34a20a948
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-11-19 23:54:16 +0000

    Refactor filtering, grouping and merging to their own namespace

--- web-science-ax/src/web_science_ax/app.clj
+++ web-science-ax/src/web_science_ax/app.clj
@@ -1,38 +1,23 @@
 (ns web-science-ax.app
   (:require [clojure.data.csv :as csv]
             [clojure.set :as set]
-           [clojure.java.io :as io]
-            [web-science-ax.io :as ax-io]))
-
-
-(defn remove-small-clusters [docMap numberOfTweets]
-  "Filters out the clusters having less tweets than numberOfTweets"
-  (into {} (filter (fn [[k v]] (> (count v) numberOfTweets)) docMap)))
-
-(defn group-clusters [docMap groupByKeyword]
-  "Groups clusters by keyword (e.g. clusterID, namedEntity)"
-  (group-by groupByKeyword docMap))
-
-(defn perform-merge [docMap windowInterval numberOfTweets]
-  "Merges the clusters who reference the same named entity and which have more than numberOfTweets tweets"
-  (filter (fn [v]
-            (> (count (flatten (vals v))) numberOfTweets))
-          (into '() (map #(group-by :cluster_id %)
-                         (vals (group-by :cluster_name_entity docMap))))))
+            [clojure.java.io :as sys-io]
+            [web-science-ax.io :as ax-io]
+            [web-science-ax.util :as util]))
 
 (defn merge-named-entities [from to windowInterval numberOfTweets]
   "Writes a CSV using the named entity merging technique"
-  (csv/write-csv (io/writer to)
+  (csv/write-csv (sys-io/writer to)
                  (map #(vals %) (flatten (map #(vals %)
-                                              (perform-merge (ax-io/read-csv->maps
+                                              (util/perform-merge (ax-io/read-csv->maps
                                                                       (csv/read-csv
-                                                                        (io/reader from)))
+                                                                        (sys-io/reader from)))
                                                                     windowInterval numberOfTweets))))))
 
 (defn filter-clusters [from to numberOfTweets]
   "Writes a CSV using the cluster filtering techniques"
-  (ax-io/write-maps->csv (remove-small-clusters
-                     (group-clusters
+  (ax-io/write-maps->csv (util/remove-small-clusters
+                     (util/group-clusters
                        (ax-io/read-csv->maps
-                         (csv/read-csv (io/reader from))) :cluster_id) numberOfTweets)
+                         (csv/read-csv (sys-io/reader from))) :cluster_id) numberOfTweets)
                    to))
---
+++ web-science-ax/src/web_science_ax/util.clj
@@ -0,0 +1,16 @@
+(ns web-science-ax.util)
+
+(defn remove-small-clusters [docMap numberOfTweets]
+  "Filters out the clusters having less tweets than numberOfTweets"
+  (into {} (filter (fn [[k v]] (> (count v) numberOfTweets)) docMap)))
+
+(defn group-clusters [docMap groupByKeyword]
+  "Groups clusters by keyword (e.g. clusterID, namedEntity)"
+  (group-by groupByKeyword docMap))
+
+(defn perform-merge [docMap windowInterval numberOfTweets]
+  "Merges the clusters who reference the same named entity and which have more than numberOfTweets tweets"
+  (filter (fn [v]
+            (> (count (flatten (vals v))) numberOfTweets))
+          (into '() (map #(group-by :cluster_id %)
+                         (vals (group-by :cluster_name_entity docMap))))))
