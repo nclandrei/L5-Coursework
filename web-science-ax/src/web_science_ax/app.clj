18203ed93c27ffdaa28d7ee18f75545e45a04cc5
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-11-19 22:38:53 +0000

    Refactor code to have 2 main methods which would be called to perform the 2 techniques

--- web-science-ax/src/web_science_ax/app.clj
+++ web-science-ax/src/web_science_ax/app.clj
@@ -6,8 +6,6 @@
 (def csvKeys [:cluster_id :cluster_name_entity
            :tweet_id :timestamp_ms :user_id :tweet_tokens :tweet_text])
 
-(def windowIntervals [450000, 900000, 1800000, 3600000, 7200000, 14400000])
-
 (defn read-csv->maps [csv-data]
   "Reads in a CSV file and outputs a map with the keys defined in csvKeys"
   (map zipmap
@@ -22,26 +20,34 @@
   (->> (map #(vals %) (flatten (vals docMap)))
        (csv/write-csv writer))))
 
-(defn filter-out-small-clusters [docMap numberOfTweets]
+(defn remove-small-clusters [docMap numberOfTweets]
   "Filters out the clusters having less tweets than numberOfTweets"
-  (into [] (vals (into {} (filter (fn [[k v]] (> (count v) numberOfTweets)) docMap)))))
+  (into {} (filter (fn [[k v]] (> (count v) numberOfTweets)) docMap)))
 
 (defn group-clusters [docMap groupByKeyword]
   "Groups clusters by keyword (e.g. clusterID, namedEntity)"
   (group-by groupByKeyword docMap))
 
-(defn compute-mean-time [docMap]
-  "Computes mean time for documents in a specific cluster"
-  (double (/ (reduce + (map (fn [x] (Long/parseLong x)) (map :timestamp_ms docMap))) (count docMap))))
-
-(defn add-centroid-times [docMap]
-  "Creates a new map with key as centroid time and value as an array of
-  the corresponding documents for that cluster"
-  (set/rename-keys docMap (zipmap (keys docMap) (map (fn [[k v]] (compute-mean-time v)) docMap))))
-
-(defn merge-named-entities [docMap windowInterval numberOfTweets]
+(defn perform-merge [docMap windowInterval numberOfTweets]
   "Merges the clusters who reference the same named entity and which have more than numberOfTweets tweets"
   (filter (fn [v]
             (> (count (flatten (vals v))) numberOfTweets))
           (into '() (map #(group-by :cluster_id %)
                          (vals (group-by :cluster_name_entity docMap))))))
+
+(defn merge-named-entities [from to windowInterval numberOfTweets]
+  "Writes a CSV using the named entity merging technique"
+  (csv/write-csv (io/writer to)
+                 (map #(vals %) (flatten (map #(vals %)
+                                              (perform-merge (read-csv->maps
+                                                                      (csv/read-csv
+                                                                        (io/reader from)))
+                                                                    windowInterval numberOfTweets))))))
+
+(defn filter-clusters [from to numberOfTweets]
+  "Writes a CSV using the cluster filtering technique"
+  (write-maps->csv (remove-small-clusters
+                     (group-clusters
+                       (read-csv->maps
+                         (csv/read-csv (io/reader from))) :cluster_id) numberOfTweets)
+                   to))
